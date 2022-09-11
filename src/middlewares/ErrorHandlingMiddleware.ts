d31b7a59b236ed78c9b677109d3d9a5cb4130c4e
SADoug <douglastec2@gmail.com>
2022-09-11 17:59:46 -0300

    deploy

--- src/middlewares/ErrorHandlingMiddleware.ts
+++ src/middlewares/ErrorHandlingMiddleware.ts
@@ -1,25 +1,25 @@
-import { Response } from "express";
+// import { Response } from "express";
 
-interface types {
-  unauthorized: number,
-  conflict: number,
-  not_found: number,
-  bad_request: number
-}
+// interface types {
+//   unauthorized: number,
+//   conflict: number,
+//   not_found: number,
+//   bad_request: number
+// }
 
 
-const ERRORS = {
-  unauthorized: 401,
-  conflict: 409,
-  not_found: 404,
-  bad_request: 400
-}
+// const ERRORS = {
+//   unauthorized: 401,
+//   conflict: 409,
+//   not_found: 404,
+//   bad_request: 400
+// }
 
-export default function errorHandlerMiddleware(err: any, res: Response) {
-  console.log(err);
-  const type: types = err.type;
-  let statusCode: number = ERRORS[type];
-  if(!statusCode) statusCode = 500; // any other types
+// export default function errorHandlerMiddleware(err: any, res: Response) {
+//   console.log(err);
+//   const type: types = err.type;
+//   let statusCode: number = ERRORS[type];
+//   if(!statusCode) statusCode = 500; // any other types
 
-  return res.sendStatus(statusCode); // internal server error
-}
+//   return res.sendStatus(statusCode); // internal server error
+// }
