3b78a60adf9a2a5a1e7268f0258199645086a302
SADoug <douglastec2@gmail.com>
2022-09-11 17:49:58 -0300

    deploy

--- src/middlewares/tokenValidation.ts
+++ src/middlewares/tokenValidation.ts
@@ -14,7 +14,7 @@ export async function tokenValidation(req: Request, res: Response, next: NextFun
   const secretKey = process.env.JWT_SECRET_KEY || "secret";
   const data: JwtPayload | Secret = jwt.verify(token, secretKey)
   if (!data) { throw { type: "conflict", message: "email already exists" } }
-  const id: JwtPayload | Secret = jwt.decode(token)
+  const id: JwtPayload | any = jwt.decode(token)
   res.locals.userId = id
 
   return next()
